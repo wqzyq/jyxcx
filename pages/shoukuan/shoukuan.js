f38bd8cf97a295d1bb771fbdc67ec547a516c9fa
wqzyq <41100288@qq.com>
2018-12-24 14:39:32 +0800

    修改文档

---
+++ pages/shoukuan/shoukuan.js
@@ -0,0 +1,23 @@
+// pages/shoukuan/shoukuan.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+
+})
